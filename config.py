5e0f8eecec6751231fa086c568fd2c5da3ccb6a8
Zercu <dekakiran540@gmail.com>
2024-09-05 18:32:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_TOKEN = "YOUR_BOT_TOKEN"  # Telegram Bot Token
+API_ID = "YOUR_API_ID"  # Telegram API ID
+API_HASH = "YOUR_API_HASH"  # Telegram API Hash
+SUDO_USERS = [123456789]  # Telegram user IDs of admins
+DATABASE_PATH = "./mulla"  # Path to the SQLite database (no extension)
