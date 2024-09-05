5fdc5a540516a962f864cff0cc1bf7f6cba54aae
Zercu <dekakiran540@gmail.com>
2024-09-05 19:31:03 +0530

    Add files via upload

---
+++ music_bot/config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_TOKEN = "YOUR_BOT_TOKEN"  # Telegram Bot Token
+API_ID = "YOUR_API_ID"  # Telegram API ID
+API_HASH = "YOUR_API_HASH"  # Telegram API Hash
+SUDO_USERS = [7410049556, 6156332908, 7503719689]  # Telegram user IDs of admins
+DATABASE_PATH = "mulla"  # Path to the SQLite database (no extension)
